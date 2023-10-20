93b6833e9d388c2207111e355bf3d1ae1ef099f6
mitaka3 <justinbituin1207@gmail.com>
2023-10-20 23:44:08 +0800

    first commit

---
+++ MovieQuote.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class MovieQuote 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("My favorite movie:Weathering with you");
+        System.out.println("Movie quote:human minds work in mysterious ways");
+        System.out.println("The character who said it:hodaka.");
+        System.out.println("Release date of the move:august 28 2019 .");
+    }
+}
